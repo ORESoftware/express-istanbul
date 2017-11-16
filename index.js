1f7e078d546a2c2415c5285115840abd47eca39f
Olegzandr VD <alex@oresoftware.com>
2017-11-15 23:19:56 -0800

    6969

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-
-
 const cov = require('istanbul-middleware');
 const express = require('express');
 const app = express();
 const path = require('path');
 
-
-const isCollectCoverage = process.env.COLLECT_COVERAGE === 'yes';
-
-if (isCollectCoverage) {
-  app.use(express.static(path.join(__dirname, 'public-coverage')));
-}
-else {
-  app.use(express.static(path.join(__dirname, 'public')));
-}
-
-if (isCollectCoverage) {
-  app.use('/coverage', cov.createHandler()); // mount istanbul middleware here
-}
-
+app.use('/coverage', cov.createHandler()); // mount istanbul middleware here
 
 app.listen(6969);
